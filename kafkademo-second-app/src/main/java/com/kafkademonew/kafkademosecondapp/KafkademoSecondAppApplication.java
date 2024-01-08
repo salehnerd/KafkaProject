e4f15c64a12f7a5865033845050f7f1914d96826
saleh mohammed <nalshibl99@gmail.com>
2024-01-08 14:38:33 +0300

    Kafka Project first Push

---
+++ kafkademo-second-app/src/main/java/com/kafkademonew/kafkademosecondapp/KafkademoSecondAppApplication.java
@@ -0,0 +1,13 @@
+package com.kafkademonew.kafkademosecondapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkademoSecondAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkademoSecondAppApplication.class, args);
+	}
+
+}
---
+++ kafkademo-second-app/src/test/java/com/kafkademonew/kafkademosecondapp/KafkademoSecondAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkademonew.kafkademosecondapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkademoSecondAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
